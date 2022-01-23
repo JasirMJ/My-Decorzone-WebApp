7c1996afb86b98ce0e9a5f2d711b47c46a37801a
adsalihac <adsalihac@gmail.com>
2022-01-24 03:40:24 +0530

    loader added

---
+++ src/components/Preloader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import gif from "../assets/images/gif/trans.gif"
+// import gif from "../assets/images/loadergif.gif"
+import gif from "../assets/gif/swing.gif"
+function Preloader() {
+    return (
+        <div className="se-pre-con">
+            <div className="pre-loader">
+                <img className="img-fluid" src={gif} alt="loading" style={{height:"100px"}} />
+            </div>
+        </div>
+    )
+}
+
+export default Preloader
+// 222248
