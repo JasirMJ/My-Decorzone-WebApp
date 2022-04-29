4fa252a3927189c83c7b154d1c6845e833a628a2
adsalihac <adsalihac@gmail.com>
2022-04-29 13:16:53 +0530

    app url added

--- src/components/AppDownloadSection.js
+++ src/components/AppDownloadSection.js
@@ -11,10 +11,10 @@ const AppDownloadSection = () => {
                 <div className="news-app-promo__section col-12 col-md-6">
                     
                     <div className="news-app-promo-subsection">
-                        <a className="news-app-promo-subsection--link news-app-promo-subsection--playstore" href="https://play.google.com/store/apps/details?id=bbc.mobile.news.ww&hl=en" target="_parent">
+                        <a className="news-app-promo-subsection--link news-app-promo-subsection--playstore" href="https://play.google.com/store/apps/details?id=com.mydecorzone" target="_parent">
                             <img className="news-app-promo__play-store" src="//news.files.bbci.co.uk/include/newsspec/19854/assets/app-project-assets/google_play_store.svg" width={161} height="auto" border={0} />
                         </a>
-                        <a className="news-app-promo-subsection--link news-app-promo-subsection--appstore" href="https://itunes.apple.com/us/app/bbc-news/id364147881?mt=8" target="_parent">
+                        <a className="news-app-promo-subsection--link news-app-promo-subsection--appstore" href="https://apps.apple.com/in/app/my-decorzone/id1621143051" target="_parent">
                             <img className="news-app-promo__app-store" src="//news.files.bbci.co.uk/include/newsspec/19854/assets/app-project-assets/ios_app_store.svg" width={161} height="auto" border={0} />
                         </a>
                     </div>
